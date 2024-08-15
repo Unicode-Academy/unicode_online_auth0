d70a262d844e582071ef2a247d52cf3fa036d2a0
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-08-15 16:50:15 +0700

    Auth0 - 12

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -4,8 +4,9 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function ChangePassword() {
   const [form, setForm] = useState({});
-  const { user, isLoading } = useAuth0();
+  const { user, isLoading, logout } = useAuth0();
   const [accessToken, setAccessToken] = useState("");
+  const [updateStatus, setUpdateStatus] = useState("idle");
   const getAccessToken = async () => {
     const response = await fetch(
       `https://${import.meta.env.VITE_AUTH0_DOMAIN}/oauth/token`,
@@ -46,6 +47,7 @@ export default function ChangePassword() {
   };
   const updatePassword = async () => {
     const id = user.sub;
+    setUpdateStatus("pending");
     const response = await fetch(
       `https://${import.meta.env.VITE_AUTH0_DOMAIN}/api/v2/users/${id}`,
       {
@@ -60,6 +62,7 @@ export default function ChangePassword() {
         }),
       }
     );
+    setUpdateStatus("idle");
     return response.ok;
   };
   const handleSubmitForm = async (e) => {
@@ -74,10 +77,16 @@ export default function ChangePassword() {
     }
     const status = await updatePassword();
     if (status) {
-      toast.success("Thay đổi mật khẩu thành công");
+      toast.success(
+        "Thay đổi mật khẩu thành công. Hệ thống sẽ tự động đăng xuất sau 2 giây"
+      );
+      setTimeout(() => {
+        logout({ logoutParams: { returnTo: import.meta.env.VITE_APP_URL } });
+      }, 2000);
     } else {
       toast.error("Thay đổi mật khẩu thất bại");
     }
+    setForm({});
   };
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -96,7 +105,24 @@ export default function ChangePassword() {
         onSubmit={handleSubmitForm}
         style={{ position: "relative" }}
       >
-        <fieldset>
+        {updateStatus === "pending" && (
+          <div
+            className="overlay"
+            id="loading"
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              opacity: "0.5",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <div className="d-flex justify-content-center">
+              <div className="spinner-border" role="status"></div>
+            </div>
+          </div>
+        )}
+        <fieldset disabled={updateStatus === "pending"}>
           <div className="mb-3">
             <label htmlFor="">Mật khẩu cũ</label>
             <input
@@ -104,6 +130,7 @@ export default function ChangePassword() {
               name="old_password"
               className="form-control"
               placeholder="Mật khẩu cũ..."
+              value={form.old_password ?? ""}
               required
               onChange={handleChange}
             />
@@ -115,6 +142,7 @@ export default function ChangePassword() {
               name="password"
               className="form-control"
               placeholder="Mật khẩu..."
+              value={form.password ?? ""}
               required
               onChange={handleChange}
             />
@@ -126,6 +154,7 @@ export default function ChangePassword() {
               name="confirm_password"
               className="form-control"
               placeholder="Nhập lại mật khẩu..."
+              value={form.confirm_password ?? ""}
               required
               onChange={handleChange}
             />
