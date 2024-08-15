f394c21ad617f7b2c4fc0b6da1e30f21e8d8c113
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-08-15 13:58:44 +0700

    Auth0 - 08

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -8,6 +8,9 @@ export default function Account() {
     useAuth0();
   const [currentUser, setCurrentUser] = useState({});
   const [form, setForm] = useState({});
+  const [userStatus, setUserStatus] = useState("pending");
+  const [updateStatus, setUpdateStatus] = useState("idle");
+  const [accessToken, setAccessToken] = useState("");
   const getAccessToken = async () => {
     const response = await fetch(
       `https://${import.meta.env.VITE_AUTH0_DOMAIN}/oauth/token`,
@@ -30,6 +33,7 @@ export default function Account() {
     if (user?.sub) {
       const id = user.sub;
       const { access_token: accessToken } = await getAccessToken();
+      setAccessToken(accessToken);
       const response = await fetch(
         `https://${import.meta.env.VITE_AUTH0_DOMAIN}/api/v2/users/${id}`,
         {
@@ -40,14 +44,12 @@ export default function Account() {
         }
       );
       const data = await response.json();
-      console.log(data);
-
       setCurrentUser(data);
+      setUserStatus("success");
     }
   };
   const updateUser = async (form) => {
     const id = user.sub;
-    const { access_token: accessToken } = await getAccessToken();
     const response = await fetch(
       `https://${import.meta.env.VITE_AUTH0_DOMAIN}/api/v2/users/${id}`,
       {
@@ -88,7 +90,9 @@ export default function Account() {
       interest,
     };
 
+    setUpdateStatus("pending");
     const status = await updateUser(rest);
+    setUpdateStatus("idle");
     if (status) {
       if (isUpdateEmailOrPhone) {
         toast.success("Cập nhật tài khoản thành công. Vui lòng đăng nhập lại.");
@@ -117,75 +121,96 @@ export default function Account() {
       interest: currentUser.user_metadata?.interest,
     });
   }, [currentUser]);
-  console.log(form);
 
-  if (isLoading) {
-    return <h3>Loading...</h3>;
-  }
   return (
     <div className="w-50 mx-auto">
       <h1>Account</h1>
-      <form action="" onSubmit={handleSubmitForm}>
-        <div className="mb-3">
-          <label htmlFor="">Tên</label>
-          <input
-            type="text"
-            name="name"
-            className="form-control"
-            placeholder="Tên..."
-            value={form?.name ?? ""}
-            onChange={handleChangeValue}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="">Email</label>
-          <input
-            type="email"
-            name="email"
-            className="form-control"
-            placeholder="Email..."
-            value={form?.email ?? ""}
-            onChange={handleChangeValue}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="">Điện thoại</label>
-          <input
-            type="text"
-            name="phone_number"
-            className="form-control"
-            placeholder="Số điện thoại..."
-            value={form?.phone_number ?? ""}
-            onChange={handleChangeValue}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="">Địa chỉ</label>
-          <input
-            type="text"
-            name="address"
-            className="form-control"
-            placeholder="Địa chỉ..."
-            value={form?.address ?? ""}
-            onChange={handleChangeValue}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="">Sở thích</label>
-          <input
-            type="text"
-            name="interest"
-            className="form-control"
-            placeholder="Sở thích..."
-            value={form?.interest ?? ""}
-            onChange={handleChangeValue}
-          />
-        </div>
-        <div className="d-grid">
-          <button className="btn btn-primary">Cập nhật</button>
-        </div>
+      <form
+        action=""
+        onSubmit={handleSubmitForm}
+        style={{ position: "relative" }}
+      >
+        {(userStatus === "pending" || updateStatus === "pending") && (
+          <div
+            className="overlay"
+            id="loading"
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              opacity: "0.5",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <div className="d-flex justify-content-center">
+              <div className="spinner-border" role="status"></div>
+            </div>
+          </div>
+        )}
+        <fieldset
+          disabled={userStatus === "pending" || updateStatus === "pending"}
+        >
+          <div className="mb-3">
+            <label htmlFor="">Tên</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Tên..."
+              value={form?.name ?? ""}
+              onChange={handleChangeValue}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Email..."
+              value={form?.email ?? ""}
+              onChange={handleChangeValue}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="">Điện thoại</label>
+            <input
+              type="text"
+              name="phone_number"
+              className="form-control"
+              placeholder="Số điện thoại..."
+              value={form?.phone_number ?? ""}
+              onChange={handleChangeValue}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="">Địa chỉ</label>
+            <input
+              type="text"
+              name="address"
+              className="form-control"
+              placeholder="Địa chỉ..."
+              value={form?.address ?? ""}
+              onChange={handleChangeValue}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="">Sở thích</label>
+            <input
+              type="text"
+              name="interest"
+              className="form-control"
+              placeholder="Sở thích..."
+              value={form?.interest ?? ""}
+              onChange={handleChangeValue}
+            />
+          </div>
+          <div className="d-grid">
+            <button className="btn btn-primary">Cập nhật</button>
+          </div>
+        </fieldset>
       </form>
       <ToastContainer />
     </div>
